763b11bc8d83ae2a68fd00e50352850080f8e5e5
fthink <fthink@naver.com>
2020-08-18 19:01:01 +0900

    - 상세 액티비티 추가

--- app/src/main/java/com/kang6264/picsumimagelist/presentation/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/kang6264/picsumimagelist/presentation/ui/detail/DetailActivity.kt
@@ -1,11 +1,13 @@
 package com.kang6264.picsumimagelist.presentation.ui.detail
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.viewModels
 import com.kang6264.picsumimagelist.R
 import com.kang6264.picsumimagelist.databinding.ActivityDetailBinding
 import com.kang6264.picsumimagelist.BR
 import com.kang6264.picsumimagelist.presentation.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : BaseActivity<ActivityDetailBinding, DetailViewModel>() {
     override val layoutId: Int = R.layout.activity_detail
@@ -18,7 +20,22 @@ class DetailActivity : BaseActivity<ActivityDetailBinding, DetailViewModel>() {
         super.onCreate(savedInstanceState)
         binding = getViewDataBinding()
 
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         val imageUrl = intent.getStringExtra("image_url")
         viewModel.imageUrl.value = imageUrl
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
     }
 }
